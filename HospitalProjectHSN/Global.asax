08b76e25654185dfb38def9b972b04383231353f
Yusha16 <chris.yushi@gmail.com>
2020-03-12 14:37:25 -0400

    initial commit

---
+++ HospitalProjectHSN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HospitalProjectHSN.MvcApplication" Language="C#" %>
---
+++ HospitalProjectHSN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalProjectHSN.Startup))]
+namespace HospitalProjectHSN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
